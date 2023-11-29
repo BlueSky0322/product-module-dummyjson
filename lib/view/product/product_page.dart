ca69ab64c7565ef3fb6d6aafd86c497c309036b2
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 01:52:06 +0800

    feat: add appbar with back button for testing

--- lib/view/product/product_page.dart
+++ lib/view/product/product_page.dart
@@ -5,6 +5,16 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
