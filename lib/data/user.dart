543c1466d88357d9d3f8248ef76f0bdd1bb8f858
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 01:42:04 +0800

    feat: add user data file with functional classes

---
+++ lib/data/user.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AuthedUser with ChangeNotifier {
+  int? id;
+  String? token;
+
+  AuthedUser({
+    this.id,
+    this.token,
+  });
+
+  factory AuthedUser.fromJson(Map<String, dynamic> json) {
+    return AuthedUser(
+      id: json['id'],
+      token: json['token'],
+    );
+  }
+
+  void setUserData(int id, String token) {
+    id = id;
+    token = token;
+    notifyListeners();
+  }
+}
+
+class UserLoginRequest {
+  final String username;
+  final String password;
+
+  UserLoginRequest({
+    required this.username,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'username': username,
+      'password': password,
+    };
+  }
+}
