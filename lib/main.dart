1aab9ade48f33f7c2f8a3bc73fafddf7fd5bded7
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 01:49:33 +0800

    fix: fix main method, added MultiProvider for AuthedUser

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:product_module/data/user.dart';
+import 'package:product_module/view/login/login_page.dart';
+import 'package:product_module/view/login/login_state.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthedUser(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +24,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home: const LoginScreen(),
+      home: ChangeNotifierProvider(
+          create: (context) => LoginPageState(context),
+          child: const LoginPage()),
     );
   }
 }
