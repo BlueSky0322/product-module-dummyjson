c8f9db96ec04da79d9dad04989a7f730c6fd341a
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 14:53:35 +0800

    feat: create state file to handle backend logic

--- lib/view/product/product_state.dart
+++ lib/view/product/product_state.dart
@@ -1,7 +1,35 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
+import 'package:product_module/data/product.dart';
+import 'package:product_module/data/user.dart';
+import 'package:product_module/services/product_service.dart';
+import 'package:provider/provider.dart';
 
 class ProductPageState extends ChangeNotifier {
   final BuildContext context;
+  bool isFirstLoadRunning = false;
+  final _productService = ProductService();
+  late AuthedUser authedUser;
+  List<ProductShort> productsList = [];
 
-  ProductPageState(this.context);
+  int skip = 0;
+  int limit = 10;
+
+  ProductPageState(this.context) {
+    // Initialize the UserProvider in the constructor
+    authedUser = Provider.of<AuthedUser>(context, listen: false);
+    // log("${authedUser.token}");
+    firstLoad();
+  }
+
+  Future<void> firstLoad() async {
+    isFirstLoadRunning = true;
+    notifyListeners();
+
+    productsList =
+        await _productService.getProducts(authedUser.token, limit, skip);
+    isFirstLoadRunning = false;
+    notifyListeners();
+  }
 }
