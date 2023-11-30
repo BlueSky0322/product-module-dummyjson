57e6fa293e78d69a65dbdb80b16c7700b575a015
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 14:52:14 +0800

    feat: add short product class for UI

--- lib/data/product.dart
+++ lib/data/product.dart
@@ -41,3 +41,35 @@ class Product {
     );
   }
 }
+
+class ProductShort {
+  final int id;
+  final String title;
+  final double price;
+  final double rating;
+  final int stock;
+  final String category;
+  final String thumbnail;
+
+  ProductShort({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.rating,
+    required this.stock,
+    required this.category,
+    required this.thumbnail,
+  });
+
+  factory ProductShort.fromJson(Map<String, dynamic> json) {
+    return ProductShort(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'].toDouble(),
+      rating: json['rating'].toDouble(),
+      stock: json['stock'],
+      category: json['category'],
+      thumbnail: json['thumbnail'],
+    );
+  }
+}
