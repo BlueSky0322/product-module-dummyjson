2b916d40b5c6222498aa915e8cad7f58f27f7254
BlueSky0322 <ryanng8628@gmail.com>
2023-11-30 14:53:08 +0800

    feat: create UI components for basic list view

--- lib/view/product/product_page.dart
+++ lib/view/product/product_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:product_module/view/product/product_state.dart';
+import 'package:provider/provider.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final state = Provider.of<ProductPageState>(context);
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -15,6 +18,29 @@ class ProductPage extends StatelessWidget {
           },
         ),
       ),
+      body: state.isFirstLoadRunning
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.productsList.length,
+                    // controller: _controller,
+                    itemBuilder: (context, index) => Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 10),
+                      child: ListTile(
+                        title: Text(state.productsList[index].title),
+                        subtitle:
+                            Text(state.productsList[index].rating.toString()),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
