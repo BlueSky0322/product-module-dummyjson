fc8ae5f8de7cb3e593cf565abe17c633f04c1e1d
BlueSky0322 <ryanng8628@gmail.com>
2023-12-01 08:44:50 +0800

    feat: create reusable widget for product page

---
+++ lib/components/ui/product_list_tile.dart
@@ -0,0 +1,106 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:product_module/data/product.dart';
+import 'package:product_module/view/product/product_details/product_details_page.dart';
+import 'package:product_module/view/product/product_details/product_details_state.dart';
+import 'package:provider/provider.dart';
+
+class ProductListTile extends StatelessWidget {
+  final ProductShort product;
+  const ProductListTile({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.blueGrey.shade100,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: ListTile(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ChangeNotifierProvider(
+                create: (context) => ProductDetailsState(context, product.id),
+                child: const ProductDetailsPage(),
+              ),
+            ),
+          );
+        },
+        leading: Image.network(
+          product.thumbnail,
+          fit: BoxFit.fitHeight,
+          width: 100,
+          height: 100,
+        ),
+        title: Text(
+          product.title,
+          style: GoogleFonts.outfit(
+            textStyle: const TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        subtitle: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.category,
+                  style: GoogleFonts.outfit(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "RM ${product.price.toStringAsFixed(2)}",
+                  style: GoogleFonts.outfit(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "${product.rating.toStringAsFixed(2)}",
+                      style: GoogleFonts.outfit(
+                        textStyle: TextStyle(
+                          color: Colors.lightBlue.shade900,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.amber.shade700,
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
